c597253f8cad25b5c4cabde353385a9ecd5f877d
anovacap <anovacap@yahoo.com>
2018-08-19 03:32:04 +0000

    loop and getfor simple shell

---
+++ shl_get_ln.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * shl_get_ln - gets input stores in array
+ * Return: nothing
+ */
+char *shl_get_ln(void)
+{
+	char *ln = NULL;
+	ssize_t bufsize = 0;
+
+	getline(&ln, &buffsize, stdin);/*add test for getline*/
+	return (ln);
+}
---
+++ shl_loop.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * shl_loop - reads input. Parses. Executes.
+ * Return: nothing
+ */
+void shl_loop(void)
+{
+	char *ln;
+	char **args;
+	int loop;
+
+	do
+	{
+		write(1, "$ ", 3); /*have to use write*/
+	/*work on proper loop while loop instead of do/while*/
+		ln = shl_get_ln();
+		args = shl_parse_ln(ln);
+		loop = shl_exe(args);
+
+		free(ln);
+		free(args);
+	} while (loop);
+}
