2b1215527492243eb024607cb486cc9cf8268d71
anovacap <anovacap@yahoo.com>
2018-08-21 00:46:01 +0000

    Add prompt.c

--- prompt.c
+++ prompt.c
@@ -22,48 +22,44 @@ char *_strdup(char *str)
 }
 int main(void)
 {
-	size_t bufsize = 1024;
-	char *buf;
+	size_t bufsize;
 	char *dup_buf;
 	char *token;
 	char *toks;
-	char **tok = NULL;
-	/*size_t characters;*/
+	char **tok;
 	int i = 1;
+	char *buf = NULL;
 
-	buf = malloc(bufsize * sizeof(char));
-	if (buf == NULL)
-		return (1);
 	write(1, "$ ", 3);
 	getline(&buf, &bufsize, stdin);
 	dup_buf = _strdup(buf);
 	printf("%s\n", dup_buf);
-	token = strtok(buf, " \n");
+	token = strtok(buf, " \n\t\r\a");
 	printf("token = %s\n", token);
 	printf("i before loop = %i\n", i);
 	while (token)
 	{
-		token = strtok(NULL, " ");
+		token = strtok(NULL, " \n\t\r\a");
 		i++;
 		printf("loop token = %s\n", token);
 	}
-	printf("after loop i = %i\n", i);
-	*tok = malloc(sizeof(char*) * i);
-	printf("XXXXXXX");
-	if (*tok == NULL)
-		return (1);
-	printf("*tok created %i * 8 bytes", i);
-	toks = strtok(dup_buf, " ");
-	tok[i] = NULL;
+	free(buf);
+	printf("i after loop i = %i\n", i);
+	tok = malloc(sizeof(char *) * i);
+	printf("XXX");
+	/*if (tok == NULL)
+	  return (1);*/
+	printf("*tok created %i * 8 bytes\n", i);
+	toks = strtok(dup_buf, " \n\t\r\a");
+	tok[i - 1] = '\0';
 	i = 0;
 	printf("%i = 0\n", i);
-	while (tok != '\0')
+	while (toks)
 	{
 		tok[i] = toks;
-		toks = strtok(NULL, " ");
+		toks = strtok(NULL, " \n\t\r\a");
 		i++;
 	}
-	/*printf("%lu characters were read.\n", characters);*/
 	printf("You typed: %s\n", *tok);
 	return (0);
 }
