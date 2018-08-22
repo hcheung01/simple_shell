78597a9523d8170d01092e4ee7d058fd932c2e92
anovacap <anovacap@yahoo.com>
2018-08-22 00:29:48 +0000

    removed pid funcs add strcmp func

--- lists.h
+++ lists.h
@@ -8,21 +8,11 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-pid_t getpid(void);
-pid_t getppid(void);
-pid_t getpidmax(void);
-char *read_line_buff(void);
-<<<<<<< HEAD
+
 void shl_loop(void);
 char *shl_get_ln(void);
 char *_strdup(char *str);
 char **shl_parse_ln(char *ln);
 int shl_start(char *args[]);
-=======
-
-/* parse functions */
-char *_strdup(char *str);
-char **shl_parse_ln(char *ln);
-
->>>>>>> 7666add1a60d0dd011c48796e40c222f1f4999e1
+int _strcmp(char *s1, char *s2);
 #endif
