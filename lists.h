0096f959558da81f6f71905f024d214aaf9eb959
anovacap <anovacap@yahoo.com>
2018-08-25 02:48:07 +0000

    updated lists.h

--- lists.h
+++ lists.h
@@ -7,9 +7,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 #define DELIM " /n/a/t"
-static char **environ;
+extern char **environ;
+/*static char **environ;*/
 
 void prompt(void);
 char *get_line(void);
@@ -20,5 +22,10 @@ char *_strdup(char *str);
 char **shl_parse_ln(char *ln);
 int shl_start(char *args[]);
 int _strcmp(char *s1, char *s2);
-
+char *_strcpy(char *dest, char *src);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int _strlen(char *s);
+char *shl_path(void);
+char **shl_path_buf(char *start);
+char *shl_stat(char **av, char *getln);
 #endif
