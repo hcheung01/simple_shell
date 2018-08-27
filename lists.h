97bec652c55ede35405291b6911703f64c81580f
anovacap <anovacap@yahoo.com>
2018-08-27 22:20:04 +0000

    merge con

--- lists.h
+++ lists.h
@@ -6,27 +6,19 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdlib.h>
 #include <sys/stat.h>
-
-#define DELIM " /n/a/t"
+#include <stdlib.h>
+extern int dircount;
 extern char **environ;
-static char **environ;
+#define DELIM " \n\a\t"
 
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
 void prompt(void);
 char *get_line(void);
 char **split_line(char *line);
-void shl_loop(void);
-char *shl_get_ln(void);
-char *_strdup(char *str);
-char **shl_parse_ln(char *ln);
-int shl_start(char *args[]);
-int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-int _strlen(char *s);
-char *shl_path(void);
-char **shl_path_buf(char *start);
-char *shl_stat(char **tokens, char *getln);
-void free_grid(int **grid, int height);
+char *get_env(void);
+char *pathCat(char *dir, char *av);
+char **dirTok(void);
 #endif
