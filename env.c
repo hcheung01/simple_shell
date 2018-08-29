1f383527553d46d6f205fd98efa915e65a04fd06
anovacap <anovacap@yahoo.com>
2018-08-29 19:23:23 +0000

    add env.c

--- env.c
+++ env.c
@@ -2,18 +2,9 @@
 
 void env(void)
 {
-<<<<<<< HEAD
-	while (*environ)
-	{
-		write(1, *environ, _strlen(*environ));
-		write(1, "\n", 1);
-		*environ++;
-=======
-        while (*environ)
 	{
 		write(1, *environ, _strlen(*environ));
 		write(1, "\n", 1);
 		environ++;
->>>>>>> a28552acb775acbb0d0ce0d7510e7de30d4f0c0e
 	}
 }
